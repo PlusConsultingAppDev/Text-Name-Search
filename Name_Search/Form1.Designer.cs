86aea444102ef8dc44ead78ee9eaf5ca8b64e93e
Dineshrajaskrn <dineshrajaskrn@gmail.com>
2017-06-28 13:13:56 -0500

    Completed Name search Application

---
+++ Name_Search/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Name_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.dgvNames = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNames)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(214, 28);
+            this.txtFirstName.Multiline = true;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(251, 44);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(214, 166);
+            this.txtLastName.Multiline = true;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(251, 44);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(214, 99);
+            this.txtMiddleName.Multiline = true;
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(251, 44);
+            this.txtMiddleName.TabIndex = 1;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(61, 37);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(76, 17);
+            this.lblFirstName.TabIndex = 3;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Location = new System.Drawing.Point(61, 117);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(90, 17);
+            this.lblMiddleName.TabIndex = 4;
+            this.lblMiddleName.Text = "Middle Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(61, 179);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 17);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // dgvNames
+            // 
+            this.dgvNames.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNames.Location = new System.Drawing.Point(49, 290);
+            this.dgvNames.Name = "dgvNames";
+            this.dgvNames.RowTemplate.Height = 24;
+            this.dgvNames.Size = new System.Drawing.Size(765, 269);
+            this.dgvNames.TabIndex = 6;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(306, 239);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 30);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 612);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvNames);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNames)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.DataGridView dgvNames;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Name_Search/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_Search
+{
+    public partial class Form1 : Form
+    {
+        public List<EmployeeData> employeesData;
+        public bool heightSet = false;
+        public Form1()
+        {
+            InitializeComponent();
+            dgvNames.Visible = false;
+            EmployeesData();
+            this.Height = this.Height - dgvNames.Height - 5;
+        }
+
+        public void EmployeesData()
+        {
+            employeesData = new List<EmployeeData>();
+            employeesData.Add(new EmployeeData { Id = 1, FirstName = "Connor", MiddleName = "Gary", LastName = "Smith" });
+            employeesData.Add(new EmployeeData { Id = 2, FirstName = "Connor", MiddleName = "Gary", LastName = "Smith" });
+            employeesData.Add(new EmployeeData { Id = 3, FirstName = "Seth", MiddleName = "David", LastName = "Greenly" });
+            employeesData.Add(new EmployeeData { Id = 4, FirstName = "Seth", MiddleName = "David", LastName = "Greenly" });
+            employeesData.Add(new EmployeeData { Id = 4, FirstName = "Seth", MiddleName = "Gary", LastName = "Greenly" });
+            employeesData.Add(new EmployeeData { Id = 4, FirstName = "Seth", MiddleName = "Warren", LastName = "Greenly" });
+            employeesData.Add(new EmployeeData { Id = 5, FirstName = "David", MiddleName = "Gary", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 6, FirstName = "David", MiddleName = "Warren", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 7, FirstName = "David", MiddleName = "Warren", LastName = "Smith" });
+            employeesData.Add(new EmployeeData { Id = 8, FirstName = "David", MiddleName = "Smith", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 8, FirstName = "David", MiddleName = "S.", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 8, FirstName = "David", MiddleName = "W.", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 8, FirstName = "David", MiddleName = "S", LastName = "Black" });
+            employeesData.Add(new EmployeeData { Id = 8, FirstName = "David", MiddleName = "W", LastName = "Black" });
+        }
+
+        public class EmployeeData
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+
+            public string MiddleName { get; set; }
+
+            public string LastName { get; set; }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtFirstName.Text) && string.IsNullOrEmpty(txtMiddleName.Text) && string.IsNullOrEmpty(txtLastName.Text))
+            {
+                MessageBox.Show("No Data Found!");
+            }
+            var employeeDataToBind = employeesData.Where(x => ((!string.IsNullOrEmpty(txtFirstName.Text) && x.FirstName.ToLower().Contains(txtFirstName.Text.ToLower())) && (!string.IsNullOrEmpty(txtMiddleName.Text) && x.MiddleName.Replace(".", "").ToLower().Contains(txtMiddleName.Text.Replace(".", "").ToLower())) && (!string.IsNullOrEmpty(txtLastName.Text) && x.LastName.ToLower().Contains(txtLastName.Text.ToLower()))) || (string.IsNullOrEmpty(txtMiddleName.Text) && (!string.IsNullOrEmpty(txtFirstName.Text) && x.FirstName.ToLower().Contains(txtFirstName.Text.ToLower())) && (!string.IsNullOrEmpty(txtLastName.Text) && x.LastName.ToLower().Contains(txtLastName.Text.ToLower())))).ToList();
+
+            var filteredEmpData = new List<FilteredEmployeeData>();
+            foreach (var employee in employeeDataToBind)
+            {
+                var filEmpData = new FilteredEmployeeData();
+                filEmpData.FirstName = employee.FirstName;
+                filEmpData.MiddleName = employee.MiddleName;
+                filEmpData.LastName = employee.LastName;
+                filteredEmpData.Add(filEmpData);
+            }
+            filteredEmpData.Add(new FilteredEmployeeData());
+            dgvNames.DataSource = filteredEmpData;
+
+            dgvNames[0, dgvNames.Rows.Count - 1].Value = "Total Sum";
+            dgvNames.Rows[dgvNames.Rows.Count - 1].Cells[0].Style.BackColor = Color.Yellow;
+            dgvNames.Rows[dgvNames.Rows.Count - 1].Cells[1].Style.ForeColor = Color.Red;
+
+            dgvNames[1, dgvNames.Rows.Count - 1].Value = (filteredEmpData.Count() - 1).ToString();
+
+            dgvNames.Refresh();
+            if (!heightSet)
+            {
+                this.Height = this.Height + dgvNames.Height + 5;
+                heightSet = true;
+            }
+
+            this.Refresh();
+            dgvNames.Visible = true;
+        }
+
+        public class FilteredEmployeeData
+        {
+            public string FirstName { get; set; }
+
+            public string MiddleName { get; set; }
+
+            public string LastName { get; set; }
+        }
+    }
+}
