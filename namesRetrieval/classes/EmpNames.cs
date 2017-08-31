6d5e67b11d24b96a2fec5e7cc7b62ff670039dd7
Paul T. Rykiel <ptrykiel@gmail.com>
2017-08-31 17:31:25 -0500

    my solution to Names Search

---
+++ namesRetrieval/classes/EmpNames.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namesRetrieval.classes
+{
+    public class EmpNames
+    {
+        private string _lname;
+        private string _fname;
+        private string _mname;
+        private string _minit;
+        public EmpNames (string fname, string mname, string lname)
+        {
+            _lname = lname;
+            _fname = fname;
+            _mname = mname;
+            _minit = mname.Substring(0, 1);
+        }
+        
+        public string retrieveFname()
+        {
+            return _fname;
+        }
+
+         
+
+        
+        public List<string> namesCollection()
+        {
+            var nameList = new List<string>();
+            nameList.Add(GetFirstLastName());
+            nameList.Add(GetFirstMidInitLastName());
+            nameList.Add(GetFirstMidInitPtLastName());
+            nameList.Add(GetFirstMiddleLastName());
+            return nameList;
+        }
+        public string GetFirstLastName()
+        {
+            return _fname + " " + _lname;
+        }
+
+        public string GetFirstMidInitLastName()
+        {
+            return _fname + " " + _minit + " " + _lname;
+        }
+
+        public string GetFirstMidInitPtLastName()
+        {
+            return _fname + " " + _minit + ". " + _lname;
+        }
+
+        public string GetFirstMiddleLastName()
+        {
+            return _fname + " " + _mname + " " + _lname;
+        }
+    }
+}
---
+++ namesRetrievalTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using namesRetrieval.classes;
+
+namespace namesRetrievalTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNameRetrieval()
+        {
+            var name1 = new EmpNames("Connor", "Gary", "Smith");
+            
+            // Test 1
+            var fandl1 = "Connor Smith";
+            var fmil1 = "Connor G Smith";
+            var fmipl1 = "Connor G. Smith";
+            var fml1 = "Connor Gary Smith";
+
+            Assert.AreEqual(fandl1, name1.GetFirstLastName());
+            Assert.AreEqual(fmil1, name1.GetFirstMidInitLastName());
+            Assert.AreEqual(fmipl1, name1.GetFirstMidInitPtLastName());
+            Assert.AreEqual(fml1, name1.GetFirstMiddleLastName());
+
+            // Test 2
+            var name2 = new EmpNames("Seth", "David", "Greenly");
+
+            var fandl2 = "Seth Greenly";
+            var fmil2 = "Seth D Greenly";
+            var fmipl2 = "Seth D. Greenly";
+            var fml2 = "Seth David Greenly";
+
+            Assert.AreEqual(fandl2, name2.GetFirstLastName());
+            Assert.AreEqual(fmil2, name2.GetFirstMidInitLastName());
+            Assert.AreEqual(fmipl2, name2.GetFirstMidInitPtLastName());
+            Assert.AreEqual(fml2, name2.GetFirstMiddleLastName());
+
+            // Test 3
+            var name3 = new EmpNames("David", "Warren", "Black");
+
+            var fandl3 = "David Black";
+            var fmil3 = "David W Black";
+            var fmipl3 = "David W. Black";
+            var fml3 = "David Warren Black";
+
+            Assert.AreEqual(fandl3, name3.GetFirstLastName());
+            Assert.AreEqual(fmil3, name3.GetFirstMidInitLastName());
+            Assert.AreEqual(fmipl3, name3.GetFirstMidInitPtLastName());
+            Assert.AreEqual(fml3, name3.GetFirstMiddleLastName());
+
+        }
+    }
+}
