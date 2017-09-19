7fcce3ade3e5845554374a1f1309f14763d97dfa
Daniel Ziegler <movezig5@gmail.com>
2017-09-19 15:45:11 -0500

    Completed code test

---
+++ Text-Name-Search/TextSearch.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TextSearch.aspx.cs" Inherits="Text_Name_Search.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Text Search</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h2>Text Search</h2>
+        <div>
+            <label for="txtSearch">Enter text to search for:</label>
+            <asp:Textbox ID="txtSearch" runat="server"></asp:Textbox>
+            <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search"/>
+        </div>
+    </div>
+    <asp:Panel ID="pnlResults" Visible="false" runat="server">
+        <h2>Results</h2>
+        <asp:Repeater runat="server" ID="rptResults">
+            <ItemTemplate>
+                <%#Eval("name") %>:
+                <%#Eval("occurrences") %>
+                <br />
+            </ItemTemplate>
+        </asp:Repeater>
+    </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Text-Name-Search/TextSearch.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Text_Name_Search
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public List<NameOccurrences> results = new List<NameOccurrences> {
+            new NameOccurrences("Connor Gary Smith", 0),
+            new NameOccurrences("Seth David Greenly", 0),
+            new NameOccurrences("David Warren Black", 0)
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                refreshResults();
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string[] lines = txtSearch.Text.Split('\n');
+            foreach (NameOccurrences name in results)
+            {
+                string[] subNames = name.name.Split(' ');
+                string firstName = subNames[0];
+                string middleName = subNames[1];
+                string middleInitial = middleName.Substring(0, 1);
+                string lastName = subNames[2];
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if (
+                            (words[i] == firstName && words[i + 1] == lastName)
+                            || (words[i] == firstName && words[i + 1] == middleInitial && words[i + 2] == lastName)
+                            || (words[i] == firstName && words[i + 1] == middleInitial + '.' && words[i + 2] == lastName)
+                            || (words[i] == firstName && words[i + 1] == middleName && words[i + 2] == lastName)
+                        ) { name.occurrences++; }
+                    }
+                }
+            }
+            refreshResults();
+            pnlResults.Visible = true;
+        }
+
+        public void refreshResults()
+        {
+            rptResults.DataSource = results;
+            rptResults.DataBind();
+        }
+    }
+
+    public class NameOccurrences
+    {
+        public string name { get; set; }
+        public int occurrences { get; set; }
+        public NameOccurrences(string name, int occurrences)
+        {
+            this.name = name;
+            this.occurrences = occurrences;
+        }
+    }
+}
