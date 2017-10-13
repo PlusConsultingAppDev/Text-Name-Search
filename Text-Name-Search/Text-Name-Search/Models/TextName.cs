6d8d1d3452c944f62bf1fa8e554f7e8c55349363
BermudaLamb <thomlamb@gmail.com>
2017-10-13 10:57:05 -0500

    Plus Consulting Code Solution

---
+++ Text-Name-Search/Text-Name-Search/Models/TextName.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Text_Name_Search.Models
+{
+    class TextName
+    {
+        #region Properties
+        public string First { get; set; }
+        public string Middle { get; set; }
+        public string Last { get; set; }
+
+        public string InitialPeriod
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Middle)
+                    ? ""
+                    : string.Format("{0}.", Middle.Substring(0, 1));
+            }
+        }
+
+        public string Initial
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Middle)
+                    ? ""
+                    : string.Format("{0}", Middle.Substring(0, 1));
+            }
+        }
+        #endregion
+
+        public TextName(string line)
+        {
+            var data = Regex.Split(line, @"\s+").ToList();
+            First = data.First();
+            Last = data.Last();
+            Middle = data.Count == 3 ? data.ElementAt(1) : "";
+            FML = string.Format($"{First} {Middle} {Last}");
+            FL = string.Format($"{First} {Last}");
+            FMIL = string.Format($"{First} {Initial} {Last}");
+            FMIPL = string.Format($"{First} {InitialPeriod} {Last}");
+        }
+
+        private int _count = 0;
+        public string FML { get; set; }
+        public string FL { get; set; }
+        public string FMIL { get; set; }
+        public string FMIPL { get; set; }
+
+        public bool Contains(string line)
+        {
+            return (line.Contains(FML)
+                || line.Contains(FMIPL)
+                || line.Contains(FMIL)
+                || line.Contains(FL));
+        }
+
+        public int Count(string line)
+        {
+            if (!Contains(line)) return _count;
+            _count += Regex.Matches(line, FML).Count
+                + Regex.Matches(line, FL).Count
+                + Regex.Matches(line, FMIL).Count
+                + Regex.Matches(line, FMIPL).Count;
+            return _count;
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+    }
+}
---
+++ Text-Name-Search/Text-Name-Search/Program.cs
@@ -0,0 +1,72 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Text_Name_Search.Models;
+
+namespace Text_Name_Search
+{
+    class Program
+    {
+
+        #region Error Logging
+        private static Logger _mLogger;
+        public static Logger MLogger
+        {
+            get
+            {
+                if (_mLogger == null)
+                {
+                    _mLogger = LogManager.GetCurrentClassLogger();
+                }
+                return _mLogger;
+            }
+            set { _mLogger = value; }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            var banner = string.Format("{0}\r\nText Name Search\r\n{0}", "".PadLeft(80, '-'));
+            MLogger.Info($"{banner}");
+            try
+            {
+                if (args == null || !args.Any() || args.Count() != 2)
+                {
+                    throw new ArgumentNullException("args", "Filename(s) not specified");
+                }
+                foreach (var arg in args)
+                {
+                    if (!File.Exists(arg))
+                        throw new FileNotFoundException("Specified file is missing", arg);
+                }
+                List<TextName> names = new List<TextName>();
+                if (File.Exists(args[0]))
+                {
+                    names = File.ReadAllLines(args[0]).ToList()
+                        .ConvertAll(line => new TextName(line));
+                }
+                using (var reader = new StreamReader(args[1]))
+                {
+                    var line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line.Trim() == "") continue;
+                        names.ForEach(name => name.Count(line));
+                    }
+                }
+                var results = names.Aggregate(new StringBuilder(), (a, b) =>
+                a.AppendFormat("{0}\t({1})\r\n", b.FML, b.Count()));
+                MLogger.Info($"{results}");
+            }
+            catch (Exception ex)
+            {
+                MLogger.Error(ex, ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
