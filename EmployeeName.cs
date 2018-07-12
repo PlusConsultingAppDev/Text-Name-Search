5144a642225ae08bc9b3b3cdb0b6c56941398c7d
pmadini1992 <prateek19922008@gmail.com>
2018-07-12 10:56:49 -0400

    Add files via upload

---
+++ EmployeeName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSearch
+{
+    public class EmployeeName
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + MiddleName + " " + LastName;
+
+        public string Initial
+        {
+            get
+            {
+                if (MiddleName != null && MiddleName.Length > 1) return MiddleName.Substring(0, 1);
+                else return " ";
+            } 
+        }
+
+    }
+}
---
+++ EmployeeNameFInd.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NameSearch
+{
+    public class EmployeeNameFind
+    {
+        public void FindNameInText(List<string> EmployeeNameList, string Text)
+        {
+            List<EmployeeName> employeeList = new List<EmployeeName>();
+
+
+            foreach (var employee in EmployeeNameList)
+            {
+                var em = employee.Split(' ','.');
+                switch (em.Length)
+                {
+                    case 1:
+                        employeeList.Add(new EmployeeName(){FirstName = em[0]});
+                        break;
+                    case 2:
+                        employeeList.Add(new EmployeeName(){FirstName = em[0], LastName = em[2]});
+                        break;
+                    case 3:
+                        employeeList.Add(new EmployeeName(){FirstName = em[0],MiddleName = em[1],LastName = em[2]});
+                        break;
+                }
+            }
+            Console.WriteLine("List of Names with occurrences");
+            foreach (var employ in employeeList)
+            {
+                Console.WriteLine("{0}({1})", employ.FullName, FindTextCount(employ, Text));
+            }
+            Console.ReadLine();
+        }
+
+        public int FindTextCount(EmployeeName employeeName, string Text)
+        {
+            int firstLast = Regex.Matches(Text.ToLower(), $"{employeeName.FirstName} {employeeName.LastName}".ToLower()).Count;
+            int firstMiddleInitialLast = Regex.Matches(Text.ToLower(), $"{employeeName.FirstName} {employeeName.Initial} {employeeName.LastName}".ToLower()).Count;
+            int firstMiddleDotLast = Regex.Matches(Text.ToLower(), $"{employeeName.FirstName} {employeeName.Initial}. {employeeName.LastName}".ToLower()).Count;
+            int firstMiddleLast = Regex.Matches(Text.ToLower(), $"{employeeName.FirstName} {employeeName.MiddleName} {employeeName.LastName}".ToLower()).Count;
+
+            return firstLast + firstMiddleInitialLast + firstMiddleDotLast + firstMiddleLast;
+        }
+
+    }
+}
