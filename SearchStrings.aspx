2246ff049f3d00c5abd42db0a0c67ad15c13a67d
Kirsten Signar <kpsignar@hotmail.com>
2018-12-05 11:56:44 -0500

    Project with a UI taking user-input fields - search text for strings that are employee names and print out the number of occurrences.

---
+++ SearchStrings.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="SearchStrings.aspx.vb" Inherits="WebApplication1.SearchStrings" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Employee Names Search</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width: 600px;">
+            In order to find an employee name and the number of occurrences within a text, enter the text content and the employee name
+            for which you are searching, in the form below.  Press "search" to display the results. 
+            The application will find names in according to any of the following formats, regardless of casing:<br /><br />
+            [First Name] [Last Name] <br />
+            [First Name] [Middle Initial] [Last Name] <br />
+            [First Name] [Middle Initial]. [Last Name] <br />
+            [First Name] [Middle Name] [Last Name] <br /><br />
+            <div style="padding: 5px;">
+                <div style="display: inline-block; vertical-align: top;">
+                    <asp:Label runat="server" ID="lblContentToSearch" Text="Text Content To Search: " Width="170"></asp:Label>
+                </div>
+                <div style="display: inline-block">
+                    <asp:TextBox runat="server" ID="txtContentToSearch" TextMode="MultiLine" Rows="6"></asp:TextBox>
+                </div>
+            </div>
+            <div style="padding: 5px;">
+                <div style="display: inline-block;">
+                    <asp:Label runat="server" ID="lblEmployeeName" Text="Employee Name: " Width="170"></asp:Label>
+                </div>
+                <div style="display: inline-block;">
+                    <asp:TextBox runat="server" ID="txtNameInput"></asp:TextBox>
+                </div>
+            </div>
+            <div style="padding: 5px;">
+                <asp:Button runat="server" ID="btnSearch" Text="Search" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SearchStrings.aspx.vb
@@ -0,0 +1,68 @@
+﻿Public Class SearchStrings
+    Inherits System.Web.UI.Page
+
+    Dim _StringFoundCount As Integer
+
+    Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        If Not IsNothing(txtNameInput) Then
+            GetNumOccurrences(txtNameInput.Text)
+            MsgBox("The name " & txtNameInput.Text & " was found " & _StringFoundCount.ToString & " times.")
+        Else
+            MsgBox("Error: textbox is missing.")
+        End If
+    End Sub
+
+    Private Sub GetNumOccurrences(_StringToFind)
+        'First parse the first, middle, and last names, and middle initial, from the user-input name field
+        Dim _split = _StringToFind.ToString.Split(" ")
+        Dim _firstName = ""
+        Dim _middleName = ""
+        Dim _lastName = ""
+        Dim _middleInitial = ""
+        If _split.Length = 2 Then
+            _firstName = _split(0)
+            _lastName = _split(1)
+        ElseIf _split.Length = 3 Then
+            _firstName = _split(0)
+            _middleName = _split(1)
+            _lastName = _split(2)
+            _middleInitial = _middleName.Substring(0, 1)
+        Else
+            MsgBox("Invalid Input. Please re-check formatting requirements and try again.")
+            Exit Sub
+        End If
+
+        'Initialize variables
+        _StringFoundCount = 0
+
+        'Initialize a list of acceptable formats into the variable _listOfAccetableStrings, defined by the following rules.
+        'The Application must find names Of the following formats, regardless of casing: 
+        '[First Name] [Last Name]
+        '[First Name] [Middle Initial] [Last Name]
+        '[First Name] [Middle Initial]. [Last Name]
+        '[First Name] [Middle Name] [Last Name]
+        Dim _listOfAccetableStrings As New List(Of String)
+        _listOfAccetableStrings.Add(_firstName & " " & _middleInitial & " " & _lastName)
+        _listOfAccetableStrings.Add(_firstName & " " & _middleInitial & ". " & _lastName)
+        _listOfAccetableStrings.Add(_firstName & " " & _lastName)
+        _listOfAccetableStrings.Add(_firstName & " " & _middleName & " " & _lastName)
+
+        'For each string in _listOfAccetableStrings, loop through every character in the text content field, 
+        'And if the first character is a match, then check for a match based on the length of the current string.
+        For Each _string In _listOfAccetableStrings
+            Dim _length = _string.Length
+            Dim _CharCounter As Integer = 0
+            For Each _char As Char In txtContentToSearch.Text.ToString
+                If _char = _StringToFind.ToString.Substring(0, 1) Then
+                    Dim _StringOfSameLength = txtContentToSearch.Text.Substring(_CharCounter, _length)
+                    If _StringOfSameLength.ToLower = _string.ToLower Then
+                        _StringFoundCount = _StringFoundCount + 1
+                    End If
+                End If
+                _CharCounter = _CharCounter + 1
+            Next
+        Next
+
+    End Sub
+
+End Class
