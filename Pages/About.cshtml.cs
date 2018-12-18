eb8d7b36f4487835566aed2050499f41cdee1481
MysteryAchievement <from.github@therickards.net>
2018-12-18 15:40:31 -0500

    nitial commit.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Text_Name_Search.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Developed for the Plus Consulting Code Test: Name Search";
+        }
+    }
+}
