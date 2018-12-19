902cb0c78a78c1bfd1091dfc3514a5cb2f1a9cb6
MysteryAchievement <from.github@therickards.net>
2018-12-19 08:47:12 -0500

    Implemented basic submit logic for URL

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,17 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Text_Name_Search.Data;
 
 namespace Text_Name_Search.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly TextNameSearchContext _db;
+        private SearchServices.Content svc;
+
+        [BindProperty]
+        public string UrlToSearch { get; set; }
+
         public void OnGet()
         {
 
         }
+
+        public IndexModel(TextNameSearchContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await 
+            var searchItems = _db.SearchItem;
+            foreach (var searchItem in searchItems)
+            {
+                //call the permutations service to add variations to our search collection
+                //
+            }
+
+            return RedirectToPage("/Index");
+        }
+
     }
 }
