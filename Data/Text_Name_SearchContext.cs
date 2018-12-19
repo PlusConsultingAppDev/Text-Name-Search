53b6fc0935fb0e80c104fc3a5f971938efea29b6
MysteryAchievement <from.github@therickards.net>
2018-12-19 08:45:22 -0500

    Performed inital migration

---
+++ Data/Text_Name_SearchContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Text_Name_Search.Data
+{
+    public class TextNameSearchContext : DbContext
+    {
+        public TextNameSearchContext (DbContextOptions<TextNameSearchContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Text_Name_Search.Models.SearchItem> SearchItem { get; set; }
+    }
+}
---
+++ Migrations/20181218222322_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Text_Name_Search.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SearchItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SearchItem", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SearchItem");
+        }
+    }
+}
