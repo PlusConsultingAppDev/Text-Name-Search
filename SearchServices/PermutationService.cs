d84029afe00de8e7158e34d7a01e938fb9d129e4
MysteryAchievement <from.github@therickards.net>
2018-12-20 23:13:43 -0500

    Renamed permutationsList; to _permutationsList

--- SearchServices/PermutationService.cs
+++ SearchServices/PermutationService.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace SearchServices
 {
     public class PermutationService
     {
-        private readonly List<string> permutationsList;
+        private readonly List<string> _permutationsList;
+        private readonly string _name;
 
         public PermutationService(string name)
         {
-            permutationsList = new List<string>();
+            _name = name;
+            _permutationsList = new List<string>();
         }
 
-        public IList GeneratePermutations(string Name)
+        public IList GeneratePermutations()
         {
-            return permutationsList;
+            //call name generator
+            NameGenerator();
+
+            return _permutationsList;
+        }
+
+        private void NameGenerator()
+        {
+            // we can add the passed in as <FirstName> <Middle Name> <LastName>
+            _permutationsList.Add(_name);
+
+            // split the name into component pieces
+            string[] fmlNames= _name.Split(' ');
+
+            // build <FirstName> <Middle Initial> <LastName>
+            // Since '+' concatenation is frowned upon
+            // we'll the string Format method here
+            _permutationsList.Add(string.Format("{0} {1} {2}", fmlNames[0], fmlNames[1][0], fmlNames[2]));
+
+            // build <FirstName> <Middle Initial><period> <LastName>
+            // we'll the string interpolation technique here - syntactic sugar for the previous
+            _permutationsList.Add($"{fmlNames[0]} {fmlNames[1][0]}. {fmlNames[2]}");
+
+            // build <FirstName> <LastName>
+            // we'll a StringBuilder here
+            StringBuilder sb = new StringBuilder().Append(fmlNames[0]).Append(" ").Append(fmlNames[2]);
+            _permutationsList.Add(sb.ToString());
+            // cleanup
+            sb.Clear();
         }
     }
 }
