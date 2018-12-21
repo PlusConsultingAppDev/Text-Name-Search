d62f2e48af7616c20a4c871629a79f8bdf6fb13e
MysteryAchievement <from.github@therickards.net>
2018-12-20 23:11:46 -0500

    Added class NameSearchService

---
+++ SearchServices/NameSearchService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchServices
+{
+    class NameSearchService
+    {
+        // so this beast with have the search string as a key to the outer dictionary
+        // the inner dictionary will keep the permutations of the search string
+        // with an integer counter.  Every time we encounter a variant, we'll increment
+        // the counter.  
+        private Dictionary<string, Dictionary<string, int>> _dictionary;
+        public NameSearchService()
+        {
+            _dictionary = new Dictionary<string, Dictionary<string, int>>();
+        }
+
+
+    }
+}
