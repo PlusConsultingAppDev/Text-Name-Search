a25b777e080da23b89f698f4f1df08757032dfdd
MysteryAchievement <from.github@therickards.net>
2019-01-02 07:07:48 -0500

    Added TestNameSearch() and general cleanup

--- NameSearchTests/SearchServiceUnitTests.cs
+++ NameSearchTests/SearchServiceUnitTests.cs
@@ -13,9 +13,13 @@ namespace Tests
 {
     public class SearchServiceUnitTests
     {
+        ContentManagementService svc;
+
         [SetUp]
         public void Setup()
         {
+            // arrange for most of the tests
+            svc = new ContentManagementService();
         }
 
         [Test]
@@ -23,8 +27,13 @@ namespace Tests
         {
             const string stringToPermutate = "Christopher Douglas Rickard";
 
-            var svc = new PermutationService(stringToPermutate);
-            var nameList = svc.GeneratePermutations();
+            // arrange
+            var permutationService = new PermutationService(stringToPermutate);
+
+            // act
+            var nameList = permutationService.GeneratePermutations();
+
+            // assert
             Assert.IsNotEmpty(nameList);
             Assert.IsTrue(nameList.Contains("Christopher Douglas Rickard"));
             Assert.IsTrue(nameList.Contains("Christopher D Rickard"));
@@ -37,10 +46,8 @@ namespace Tests
         {
             const string stringToFind = "pulvinar turpis laoreet dictum ultrices. Aenean";
 
-            // fire up an instance
-            var svc = new ContentManagementService();
 
-            // fetch the text from the website
+            // Act: fetch the text from the website
             var context = svc.FetchPage("https://github.com/PlusConsultingAppDev/Text-Name-Search").ToString().ToLower();
 
             // assert that our text is in the fetched string
@@ -52,10 +59,7 @@ namespace Tests
         {
             const string stringToFind = "Curabitur viverra eget";
 
-            // fire up an instance
-            var svc = new ContentManagementService();
-
-            // fetch the text from the website
+            // Act: fetch the text from the website
             var content = await svc.FetchPageAsync("https://github.com/PlusConsultingAppDev/Text-Name-Search");
             var contentToSearch = content.ToString().ToLower();
 
@@ -66,18 +70,48 @@ namespace Tests
         [Test]
         public async Task TestNameSearch()
         {
-            // fire up an instance
-            var svc = new ContentManagementService();
 
-            var resultsList = new List<string>();
+            // more arranging
+            var resultsList = new List<KeyValuePair<string, int>>();
+            var nameSearchService = new NameSearchService();
 
-            // fetch the text from the website
+            // Act: fetch the text from the website
             var contentToSearch = await svc.FetchPageAsync("https://github.com/PlusConsultingAppDev/Text-Name-Search");
-            var nameSearchService = new NameSearchService(contentToSearch);
-            nameSearchService.AddSearchName("Connor Gary Smith");
-            var listOfNamesFound = nameSearchService.SearchResults();
 
+            // add our test names
+            nameSearchService.AddSearchName("Connor Gary Smith");
+            nameSearchService.AddSearchName("Seth David Greenly");
+            nameSearchService.AddSearchName("David Warren Black");
+
+            // find results in the content to search
+            var listOfNamesFound = nameSearchService.SearchResults(contentToSearch);
+
+            // first we can asset that we found names
             Assert.IsNotEmpty(listOfNamesFound);
+            
+            // Assert: should get 5 results for each of the three names
+            Assert.AreEqual(listOfNamesFound.Count,15);
+
+            // Assert the counts are correct.
+            // Each one should be one more than indicated on the exercise's because the description contains
+            // each the name in the description plus those in content to search
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Connor Gary Smith", 1)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Connor G Smith", 1)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Connor G. Smith", 0)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Connor Smith", 2)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Connor Gary Smith total", 4)), true);
+
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Seth David Greenly", 2)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Seth D Greenly", 1)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Seth D. Greenly", 1)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Seth Greenly", 0)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("Seth David Greenly total", 4)), true);
+
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("David Warren Black", 2)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("David W Black", 0)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("David W. Black", 1)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("David Black", 2)), true);
+            Assert.AreEqual(listOfNamesFound.Contains(new KeyValuePair<string, int>("David Warren Black total", 5)), true);
         }
     }
 }
