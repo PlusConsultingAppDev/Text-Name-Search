f2ab4ba0f8cf514eb824b191af2fdccb42b8301b
MysteryAchievement <from.github@therickards.net>
2019-01-04 09:03:52 -0500

    Added data annotations to make Name field required and to vaslidate input via a regular expression.

--- Text_Name_Search/Models/SearchItem.cs
+++ Text_Name_Search/Models/SearchItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace Text_Name_Search.Models
     public class SearchItem
     {
         public int ID { get; set; }
+
+        // regex to get first, middle, last names (min. 2 chars long) separated by spaces
+        [RegularExpression(@"[a-zA-z]{2,}\s[a-zA-z]{2,}\s[a-zA-z]{2,}", ErrorMessage = "Please enter first name, middle name, last name separated by spaces.  Each name must be at least 2 chars long.")]
+        [Required]
         public string Text { get; set; }
     }
 }
